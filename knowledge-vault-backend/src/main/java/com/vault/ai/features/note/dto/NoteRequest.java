59e916a356e2816f126f3ac981de6ecf8a9d25e1
Prajwalkadam29 <prajwal.kadam2005@gmail.com>
2026-01-06 03:29:51 +0530

    Added Note Data Transfer Object

---
+++ knowledge-vault-backend/src/main/java/com/vault/ai/features/note/dto/NoteRequest.java
@@ -0,0 +1,13 @@
+package com.vault.ai.features.note.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class NoteRequest {
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Content is required")
+    private String content;
+}
