f47e77932c04da1c24fcf8ba08e1450135d000d9
Prajwalkadam29 <prajwal.kadam2005@gmail.com>
2026-01-06 03:21:51 +0530

    Initial commit

---
+++ knowledge-vault-backend/src/test/java/com/vault/ai/KnowledgeVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vault.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
